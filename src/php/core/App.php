70412cb6d8dcc159c76a459bbcc0bb0b1ed589f0
prostoAE <trident998@gmail.com>
2019-04-06 09:37:41 +0300

    session_start fix

--- src/php/core/App.php
+++ src/php/core/App.php
@@ -7,8 +7,8 @@ class App {
   public static $app;
 
   public function __construct() {
-    session_abort();
-    $query = trim(substr($_SERVER['REQUEST_URI'], 13), '/');
+    session_start();
+    echo $query = trim(substr($_SERVER['REQUEST_URI'], 13), '/');
     Router::dispatch($query);
   }
 
