2616a6e42b6ec0fdac574256e504b307c3397732
prostoAE <trident998@gmail.com>
2019-05-14 17:38:08 +0300

    some fix

--- src/php/classes/controllers/MainController.php
+++ src/php/classes/controllers/MainController.php
@@ -2,15 +2,11 @@
 
 namespace php\classes\controllers;
 
-use php\classes\models\User;
-
 class MainController extends AppController {
 
   public function indexAction() {
     require dirname(ROOT) . '/vendor/autoload.php';
     $this->setMeta('Главная страница', 'Система управления промо', 'Promo, sup, тарифы');
-//    debug(User::getUserAccess());
-//    debug(User::getUserLink());
   }
 
 }
